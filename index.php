88620f02221d23067e6bda343b64a9131729eb97
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-08 15:39:02 +0200

    milestone 1

--- index.php
+++ index.php
@@ -32,6 +32,12 @@ Dare all’utente anche la possibilità di permettere o meno la ripetizione di c
         </form>
 
     <?php
+     if (isset($_GET['length'])) {
+        $passwordLength = $_GET['length'];
+        $password = generatePassword($passwordLength);
+        echo "<h2>La tua password generata:</h2>";
+        echo "<p>$password</p>";
+    }
     function generatePassword($length) {
         $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?';
         $password = '';
