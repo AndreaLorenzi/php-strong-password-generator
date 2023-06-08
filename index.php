07539ec3fa6396e7a311bb2e316d109015eecee3
AndreaLorenzi <124718024+AndreaLorenzi@users.noreply.github.com>
2023-06-08 15:10:31 +0200

    aggiunto form

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!-- Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+Milestone 1
+Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+Milestone 3 (BONUS)
+Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
+Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. -->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,9 +17,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" defer></script>
+    <title>generatore di password</title>
 </head>
 <body>
+<div class="container">
+        <h1>Generatore di password sicure</h1>
+        <form action="index.php" method="GET">
+            <div class="form-group">
+                <label for="length">Lunghezza password:</label>
+                <input type="number" id="length" name="length" class="form-control" min="1" max="100" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Genera password</button>
+        </form>
+
     
 </body>
 </html>
